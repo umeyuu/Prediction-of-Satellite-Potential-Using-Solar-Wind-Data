7c24881972486dcc85af93004c730ba16ffee191
umeda yusuke <umedayusuke@TempnoMac-mini.local>
2022-12-01 17:44:43 +0900

    更新

--- dataset.py
+++ dataset.py
@@ -19,14 +19,21 @@ class MyDataset(Dataset):
         for path in paths:
             df_tmp = self.load_data(path, df)
             df = df_tmp
-        df = df[df.flow_speed != 99999.898438]
-        self.df = df
+        # df = df[df.flow_speed != 99999.898438]
+        df = df.reset_index(drop=True)
+        self.x_time = df.date
+        self.df_solar = df.copy()
+        df.drop('date', axis=1, inplace=True)
+
+        df_sat = pd.read_csv('DATA/dmsp-f16/dmsp-f16_data.csv', parse_dates=["date"])
+        df_sat = df_sat.sort_values('date')
+        self.df_sat = df_sat.reset_index(drop=True)
+        self.target_time = self.df_sat.date
 
         # 正規化処理
         scaler = MinMaxScaler()
         self.train_array = scaler.fit_transform(df)
         
-        self.date = df.index
         self.input_size = len(df)
         self.window_size = window_size
         
@@ -34,7 +41,12 @@ class MyDataset(Dataset):
         return self.input_size
     
     def __getitem__(self, index):
-        return self.train_array[index : index+self.window_size]
+        time = self.target_time[index]
+        y = self.df_sat[index:index+1].values
+        i = self.x_time[self.x_time == time].index.item() + 1
+        x = self.train_array[i-self.window_size : i]
+
+        return np.array(x), y[0][1:]
     
     def load_data(self, path, df):
         # cdfファイル読み込み
@@ -49,17 +61,20 @@ class MyDataset(Dataset):
         epoch = cdflib.cdfepoch.unixtime(cdf_file['Epoch'])
         date = [datetime.utcfromtimestamp(e) for e in epoch]
 
-        df_tmp = pd.DataFrame(tmp.T, columns=X, index=date)
+        df_tmp = pd.DataFrame(tmp.T, columns=X)
+        df_tmp['date'] = date
 
         return pd.concat([df, df_tmp])
 
-
+file_lis = []
 for year in range(2010, 2015):
     dir = f'DATA/solar_wind/{year}/'
     files = os.listdir(dir)
-    file_lis = [dir+f for f in files if os.path.isfile(os.path.join(dir, f))]
-    file_lis = sorted(file_lis)
-dataset = MyDataset(file_lis, 5)
+    tmp = [dir+f for f in files if os.path.isfile(os.path.join(dir, f))]
+    tmp = sorted(tmp)
+    file_lis.extend(tmp)
+
+dataset = MyDataset(file_lis, 60)
 print(dataset[0])
 breakpoint()
 
